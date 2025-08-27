63ae25d08d37fbb8faade45c24191468ba94bc63
nowzycc <wangzhenyuan@ihep.ac.cn>
2025-08-27 21:48:44 +0800

    增加了测试添加job的函数

--- test/test.py
+++ test/test.py
@@ -1,3 +1,10 @@
+import sys
+import os
+import subprocess
+from threading import Thread
+import yaml
+import json
+import time
 
 Joblens_path = "/home/nowzycc/code/JobLens_cpp/build/JobLens"
 Config_path = "/home/nowzycc/code/JobLens_cpp/config/config.yaml"
@@ -5,8 +12,7 @@ exec_path = "/usr/bin/sleep"
 exec_args = ["10"]
 
 def smoke_test():
-    import os
-    import subprocess
+    
 
     # 检查 Joblens 是否存在
     if not os.path.exists(Joblens_path):
@@ -17,7 +23,8 @@ def smoke_test():
     try:
         exec_cmd = f"{Joblens_path} -c {Config_path} -e {exec_path} -a {' '.join(exec_args)}"
         print(f"执行命令: {exec_cmd}")
-        result = subprocess.run(exec_cmd, shell=True, capture_output=True, text=True)
+
+        result = subprocess.run(exec_cmd, shell=True, stdout=sys.stdout, stderr=sys.stderr, text=True)
         if result.returncode != 0:
             print(f"Joblens 执行失败: {result.stderr}")
             return False
@@ -26,6 +33,33 @@ def smoke_test():
     except Exception as e:
         print(f"执行 Joblens 时发生错误: {e}")
         return False
-    
+
+def add_job_test():
+    correct_job_info = {
+        'JobID':2,
+        'JobPIDs':[1],
+        'JobCreateTime':'2025-08-27 14:33:00',
+    }
+    err_job_info = {
+        'JobID':3,
+        'JobPIDs':[],
+        'JobCreateTime':-1,
+    }
+    config = yaml.safe_load(open(Config_path))
+    job_adder_path = config['collectors_config']['job_adder_fifo']
+    def do_work():
+        exec_cmd = f"{Joblens_path} -c {Config_path} -e {exec_path} -a {' '.join(exec_args)}"
+        print(f"执行命令: {exec_cmd}")
+        result = subprocess.run(exec_cmd, shell=True, stdout=sys.stdout, stderr=sys.stderr, text=True)
+    t = Thread(target=do_work)
+    t.start()
+    time.sleep(2)  # 等待 Joblens 启动
+    with open(job_adder_path, 'w') as f:
+        f.write(json.dump(correct_job_info))
+        f.flush()
+        time.sleep(1)
+        f.write(json.dump(err_job_info))
+        f.flush()
+
 if __name__ == "__main__":
-    smoke_test()
+    add_job_test()
